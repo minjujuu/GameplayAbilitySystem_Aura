1581a6bfadc420f7f2f8ef76a9692c1e909f427d
minjujuu <aaaminju@konkuk.ac.kr>
2026-03-11 23:34:56 +0900

    9~10강: EnhancedInput과 PlayerController
    - IA_Move을 Axis 2D로 만들었다.
    - InputMappingContext에서 IA_Move에 대한 키 입력으로 WASD키를 맵핑시켰다.
    - A,S에 대해서는 음수값이 들어올 수 있도록 'Negate' 모디파이어를 적용했다.
    - W,S에 대해서는 1D 입력을 2D Action으로 바꾸어주기 위해 XYZ 입력을 YXZ로 바꾸어주는 'Swizzle Input Axis Value' 모디파이어를 적용했다.

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,29 @@
+// Copyright Minjujuu.
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	
+	Subsystem->AddMappingContext(AuraContext, 0);
+	
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	
+	FInputModeGameAndUI InputMode;
+	InputMode.SetHideCursorDuringCapture(false);
+	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	SetInputMode(InputMode);
+}
